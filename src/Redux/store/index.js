c38c4c8f71b52f9c163e9f15be7a516ab5d0e51d
Bruno Capobianco <bruno.capobianco@outlook.it>
2024-02-16 19:23:20 +0100

    redux persistant

--- src/Redux/store/index.js
+++ src/Redux/store/index.js
@@ -1,20 +1,31 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
 
-import { configureStore, combineReducers } from '@reduxjs/toolkit'
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
 import userReducer from '../reducers/users'
 import menuReducer from '../reducers/menu'
 import prenotazioniReducer from '../reducers/prenotazioni'
 import deliveryReducer from '../reducers/delivery'
 
-const bigReducer=combineReducers({
- 
- users:userReducer,
- menu:menuReducer,
- prenotazioni:prenotazioniReducer,
- delivery:deliveryReducer
 
-})
-const store=configureStore({
-    reducer:bigReducer,
-})
-export default store
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+
+
+const bigReducer = combineReducers({
+  users: userReducer,
+  menu: menuReducer,
+  prenotazioni: prenotazioniReducer,
+  delivery: deliveryReducer,
+});
+const persistedReducer = persistReducer(persistConfig, bigReducer);
+const store = configureStore({
+  reducer: persistedReducer,
+});
+
+const persistor = persistStore(store);
+
+export  { store, persistor };
